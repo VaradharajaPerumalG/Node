dcf61dcf73484697d669814371a672abb89c936b
VaradharajaPerumalG <varadharajperumalg.cs@gmail.com>
2025-04-15 17:07:48 +0530

    feat: Route Structure

--- main.js
+++ main.js
@@ -6,6 +6,28 @@ app.get("/", (req, res) => {
     res.json({msg: "Hello students!"});
 });
 
+// CRUD functionality 
+
+// R - For Reading
+app.get('/movies', () => {
+
+});
+
+// C - For Creating 
+app.post('/movies', () =>{
+
+});
+
+// U - For Updating
+app.put('/movie/:id', () => {
+
+});
+
+// D -  For Deleting
+app.delete('/movie/:id', () => {
+
+});
+
 app.listen(PORT, () => {
     console.log(`The server is running at http://localhost:6969`);
 });
