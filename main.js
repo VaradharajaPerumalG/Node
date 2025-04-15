91bca56fac2686f8b7973e560f9d33f021c69633
VaradharajaPerumalG <varadharajperumalg.cs@gmail.com>
2025-04-15 16:52:25 +0530

    feat: app structure

---
+++ main.js
@@ -0,0 +1,11 @@
+const express =  require('express')
+const app = express()
+const PORT = 6969 
+
+app.get("/", (req, res) => {
+    res.json({msg: "Hello students!"});
+});
+
+app.listen(PORT, () => {
+    console.log(`The server is running at http://localhost:6969`);
+});
